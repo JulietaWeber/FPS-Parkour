254078070acf672a4bd589782e0b8f5ef0c49661
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2025-05-27 12:05:08 -0300

    Player

--- Assets/Scripts/DeteccionColisiones.cs
+++ Assets/Scripts/DeteccionColisiones.cs
@@ -6,7 +6,8 @@ public class DeteccionColisiones : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Contacto");
+        Debug.Log("Contacto con " + collision.gameObject.name);
+        // collision es al de abajo, si no solo, es el que cae
         Destroy(collision.gameObject);
     }
 }
