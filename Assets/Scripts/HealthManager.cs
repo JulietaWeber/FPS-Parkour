0239430d76899c50d08f9c798217ae0edc4064cb
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2025-07-08 12:02:01 -0300

    Update HealthManager.cs

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playerHealth;
+    public UIHealth uiHealth;
+
     void Start()
     {
-        
+        uiHealth.UpdateHealthUI(playerHealth.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool UpdateHealth(float amount)
     {
         
+        if (playerHealth + amount < 0)
+        {
+            //no cambia nada
+            return false;
+        }
+        else
+        {
+            playerHealth += amount;
+            uiHealth.UpdateHealthUI(playerHealth.ToString());
+
+            // Si la vida llega a 0, se reinicia el personaje a la posición inicial
+            if (playerHealth <= 0)
+            {
+                SceneManager.LoadScene("FPS Parkour");
+            }
+
+            return true;
+        }
     }
 }
